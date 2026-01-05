3f1cddff75f782d57268a174a80fdc6a45906c5c
Ella Bowker <67073210+ladymacaroon@users.noreply.github.com>
2026-01-05 20:50:05 +0000

    problem 4 (with bugs)

--- problem4.py
+++ problem4.py
@@ -5,5 +5,20 @@ import string
 cases = int(sys.stdin.readline().rstrip())
 
 for caseNum in range(cases):
-   line = sys.stdin.readline().rstrip()
-   
+    line = sys.stdin.readline().rstrip()
+    letters = []
+    special = []
+
+    for x in range(len(line)):
+        letters.append(line[x])
+        
+    for x in range(len(letters)):
+        try:
+            int(letters[x]) or str(letters[x])
+        except:
+            special.append(letters[x])
+
+    for x in range(len(special)):
+        letters.remove(special[x])
+
+    print(letters)
